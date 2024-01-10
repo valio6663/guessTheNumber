5ea9146d9430319a5cfe5467e8c029e03d1d1c63
ValentinMarinov <valio6663@abv.bg>
2024-01-10 21:29:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,14 +12,18 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '😢 No Number!';
+    displayMessage('😢 No Number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🤩 Correct Number!';
+    displayMessage('🤩 Correct Number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -28,22 +32,15 @@ document.querySelector('.check').addEventListener('click', function () {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
     }
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Number is too high!';
+      displayMessage(
+        guess > secretNumber ? 'Number is too high!' : 'Number is too low!'
+      );
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '😒 You lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Number is too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😒 You lost the game';
+      displayMessage('😒 You lost the game');
       document.querySelector('.score').textContent = 0;
     }
   }
